10e8be9dbdb0be5fd44295fbb18a3e4e7c28dd9d
TUSHAR SINGH <139704706+tushars69@users.noreply.github.com>
2025-07-21 14:53:11 +0530

    Update setup

--- setup1.py
+++ setup1.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+REPO_NAME = "Kidney_Disease_Classification"
+AUTHOR_USER_NAME = "Tushar Singh"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "tusharsingh8734@gmail.com"
 
 
 setuptools.setup(
